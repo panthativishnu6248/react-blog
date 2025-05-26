9ca5a312f7aa0f32e7a3ff5ab6de3a45e5310b8e
panthativishnu6248 <panthativishnu6248@gmail.com>
2025-05-26 18:26:19 +0530

    Added all the Required files for React
    Uploaded all the required files into Repo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+
+const App = () =>{
+  const[counter,SetCounter]=useState(0);
+
+  return (
+    
+      <div className='App'>
+        
+        <button onClick={() => SetCounter((prevCount) => prevCount-1)}>-</button>
+        <h1>{counter}</h1>
+        <button>+</button>
+        <h1>
+          Hey Hi !!!
+        </h1>
+      </div>
+  );
+}
+
+export default App;
